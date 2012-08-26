45b1caec443003854588b8c06b7d3d5215a0ed90
Philipp Rieber <p.rieber@gmx.de>
2012-08-26 22:13:27 +0200

    update commands

--- webserver.pp
+++ webserver.pp
@@ -2,12 +2,13 @@ user { 'onemedia':
   ensure     => present,
   shell      => '/bin/bash',
   home       => '/home/onemedia',
+  managehome => true,
 }
 
-#file { 'testfile':
-#  path    => '/home/onemedia/testfile',
-#  ensure  => present,
-#  mode    => 0666,
-#  content => "#!/bin/bash\necho 'I am a test file'",
-#  require => User['onemedia']
-#}
+file { 'testfile':
+  path    => '/home/onemedia/testfile',
+  ensure  => present,
+  mode    => 0666,
+  content => "#!/bin/bash\necho 'I am a test file'",
+  require => User['onemedia']
+}
