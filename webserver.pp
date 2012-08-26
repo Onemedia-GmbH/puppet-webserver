db8c243b2eaedc9844b6f7bc42be6a8b24f8fd77
Philipp Rieber <p.rieber@gmx.de>
2012-08-26 21:42:51 +0200

    add test manifest

---
+++ webserver.pp
@@ -0,0 +1,13 @@
+user { 'onemedia':
+  ensure     => present,
+  shell      => '/bin/bash',
+  home       => '/home/onemedia',
+}
+
+file { 'testfile':
+  path    => '/home/onemedia/testfile',
+  ensure  => present,
+  mode    => 0666,
+  content => "#!/bin/bash\necho 'I am a test file'",
+  require => User['onemedia']
+}
